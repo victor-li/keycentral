191e0b5cfe3dd93116034ac78ba5f7b4ee2a4a78
Victor Li <victor@victorli.nl>
2017-04-08 17:06:21 +0200

    Add validation to user schema

--- app/users/routes.js
+++ app/users/routes.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var user = require('./controller');
 
 router.post('/register', function(req, res) {
-  console.log(req.body);
   user.createUser(req.body.username, req.body.password_hash, function(err) {
     if (err) {
       res.json({'success': 0, 'error': err})
--- app/users/schema.js
+++ app/users/schema.js
@@ -1,8 +1,29 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  username: { type: String, index: true },
-  password_hash: { type: String },
+  username: {
+    type: String,
+    index: true,
+    validate: {
+      validator: function(v) {
+        return /[a-z0-9]([\-\_]?[a-z0-9])*/.test(v) && /.{1,40}/.test(v);
+      },
+      message: '{VALUE} is not a valid username'
+    },
+    required: [true, 'Username is required']
+  },
+
+  password_hash: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /[a-f0-9]{64}/.test(v); // PBKDF2 hash 256 bits
+      },
+      message: 'Password hash is not valid'
+    },
+    required: [true, 'Password hash is required']
+  },
+
   vault: { type: Buffer },
   created_at: { type: Date },
   updated_at: { type: Date }
